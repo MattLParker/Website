edd5bb48838d64db229a7dc49ad6aa75c70dec10
MattLParker <MLParker1@gmail.com>
2019-01-29 17:51:15 -0500

    test upload

--- Build.bat
+++ Build.bat
@@ -1,5 +1,5 @@
 cd C:/Users/Matt/Documents/Github/www.elusivecode.com-source/
 bundle exec jekyll clean
-bundle exec jekyll build
+bundle exec jekyll build destination:C:\Users\Matt\Documents\Github\MattLParker.github.io
 
 
