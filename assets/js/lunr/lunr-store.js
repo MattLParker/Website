2197f49a2de5a1d1103bf28fad09c4bc0325b70c
MattLParker <MLParker1@gmail.com>
2019-01-29 13:41:25 -0500

    Initial Upload

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,6 @@
+var store = [{
+        "title": "Internal Nat Design",
+        "excerpt":"    Time has come where a service that we run is being moved to a remote (read not ours) datacenter. We do not know all the internal workings of it as it is a Mainframe Z9, in which we no longer have any programmers on staff. We are having the services...","categories": ["Networking"],
+        "tags": [],
+        "url": "http://localhost:4000/networking/Internal-Nat-Design/",
+        "teaser":null}]
