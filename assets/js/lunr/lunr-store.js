725130867918b9397c0352b75568ee9d1e72688e
MattLParker <MLParker1@gmail.com>
2019-01-29 18:13:21 -0500

    well known 2

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -2,20 +2,20 @@ var store = [{
         "title": "DNS Forwarders",
         "excerpt":"    When setting up or maintaining multiple AD domains, due to legacy environments. It is not wise to point the DNS servers of the new domain to the old servers.     I recently discovered this done on a 1700 user AD environment. There was a constant slow to resolve DNS when browsing...","categories": ["Networking"],
         "tags": [],
-        "url": "www.elusivecode.net/networking/DNS-Forwarders/",
+        "url": "www.elusivecode.net/.well-known/security.txt",
         "teaser":null},{
         "title": "Internal Nat Design",
         "excerpt":"    Time has come where a service that we run is being moved to a remote (read not ours) datacenter. We do not know all the internal workings of it as it is a Mainframe Z9, in which we no longer have any programmers on staff. We are having the services...","categories": ["Networking"],
         "tags": [],
-        "url": "www.elusivecode.net/networking/Internal-Nat-Design/",
+        "url": "www.elusivecode.net/.well-known/security.txt",
         "teaser":null},{
         "title": "Skype for Business Desktop sharing disconnect.",
         "excerpt":"    During testing of using Skype for business for Audio/Video conferencing all was going well, until enabling desktop sharing. When ever anyone in the group (all on LAN to each other) we would start a storm of disconnects from Skype.     While troubleshooting the problem, it was noticed that the person who...","categories": ["Networking"],
         "tags": [],
-        "url": "www.elusivecode.net/networking/Skype-Disconnect/",
+        "url": "www.elusivecode.net/.well-known/security.txt",
         "teaser":null},{
         "title": "Speeding up System Center OS Deployment",
         "excerpt":"    Some of the goals of the exercise was to add some automation to a task sequence and also speed up the results so that to not hinder the technicians that do the deployments.     We wanted to add bit-locker encryption to the task sequence, with full disk encryption to add security...","categories": ["Networking"],
         "tags": [],
-        "url": "www.elusivecode.net/networking/SCCM-OSD-Speed/",
+        "url": "www.elusivecode.net/.well-known/security.txt",
         "teaser":null}]
