e989657af7c17dcce543e5621b1bb539e83cee61
Matt <MLParker1@gmail.com>
2019-02-12 10:01:20 -0500

    change githandler links with User

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -14,9 +14,9 @@ export const onRouteUpdate = () => {
           card.innerHTML = `
           <div class="card">
             <div class="main">
-              <div class="user"><img class="avatar" src="${data.owner.avatar_url}" width="50" height="50"/>
+              <div class="user"><a href=${data.owner.html_url}><img class="avatar" src="${data.owner.avatar_url}" width="50" height="50"/></a>
                 <div class="user-details">
-                  <h3><a href=${card.dataset.url} target="_blank">${data.name}</a></h3>
+                  <h3><a href="https://github.com/${data.full_name}" target="_blank">${data.name}</a></h3>
                   <p class="desc">${data.description || 'Visit the repo for more info...'}</p>
                 </div>
               </div>
