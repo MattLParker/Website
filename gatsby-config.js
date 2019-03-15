65d3c7692ac573e6079ec5c0d3c51f1c7ff6be88
Matt <MLParker1@gmail.com>
2019-03-15 16:27:37 -0400

    GA start

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,6 +26,24 @@ module.exports = {
       },
     },
     {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "131757999",
+        // Puts tracking script in the head instead of the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        exclude: ["/preview/**", "/do-not-track/me/too/"],
+        // Enables Google Optimize using your container Id
+        sampleRate: 5,
+        siteSpeedSampleRate: 10,
+        cookieDomain: "elusivecode.net",
+      },
+    },
+    {
       resolve: `gatsby-transformer-remark`,
       options: {
         plugins: [
